8b5130bf58f516a9713a12cd5dfc42cfd5db72f0
lele-m1 <130391358+lele-m1@users.noreply.github.com>
2023-09-17 21:06:15 +0200

    add HTML, CSS, IMG

---
+++ javascript.js
@@ -0,0 +1,9 @@
+document.addEventListener("DOMContentLoaded", function () {
+    const menuToggle = document.querySelector(".menu-toggle");
+    const navList = document.querySelector("nav ul");
+
+    menuToggle.addEventListener("click", function () {
+        navList.classList.toggle("active");
+        menuToggle.classList.toggle("active");
+    });
+});
